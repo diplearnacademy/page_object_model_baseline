ea6a8e063bfaf5518e6d86e29ec017e9aa3f2dfc
Bryan Estefan Moreno Diaz <estefandx@hotmail.com>
2024-01-21 15:30:49 -0500

    actualizar rama main proyecto base

--- src/test/java/co/dlacademy/AirbnbReserve.java
+++ src/test/java/co/dlacademy/AirbnbReserve.java
@@ -4,96 +4,22 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-public class AirbnbReserve extends  BaseTest{
-
-
+public class AirbnbReserve   {
+    
     @Test
-    void reserve() throws InterruptedException {
-
-        WebElement openSearch =  driver.findElement(By.xpath("//div[@data-testid='little-search']/span/button/div[text()='En cualquier lugar del mundo']/.."));
-
-        if (openSearch.isDisplayed()){
-            openSearch.click();
-        }
-
-        WebElement selectPlace = driver.findElement(By.xpath("//input[@data-testid='structured-search-input-field-query']"));
-        selectPlace.sendKeys("Santa marta");
-        selectPlace.sendKeys(Keys.ENTER);
-
-        //selecionar fechas inicial
-        WebElement startDate = driver.findElement(By.xpath("//div[@aria-label='Calendario']/.//h2[text()='Diciembre 2023']/ancestor::div[@data-visible]"));
-        System.out.println(startDate.getAttribute("data-visible"));
-
-        while (!startDate.getAttribute("data-visible").equals("true")){
-            Thread.sleep(3000);
-            driver.findElement(By.xpath("//button[@aria-label='Haz clic en la flecha de la derecha para cambiar al mes siguiente.']")).click();
-        }
-
-        driver.findElement(By.xpath("//td/div[contains(@data-testid,'15/12')]")).click();
-
-        // fecha final
-        WebElement endDate = driver.findElement(By.xpath("//div[@aria-label='Calendario']/.//h2[text()='Diciembre 2023']/ancestor::div[@data-visible]"));
-        System.out.println(startDate.getAttribute("data-visible"));
-
-        while (!endDate.getAttribute("data-visible").equals("true")){
-            Thread.sleep(3000);
-            driver.findElement(By.xpath("//button[@aria-label='Haz clic en la flecha de la derecha para cambiar al mes siguiente.']")).click();
-        }
-
-        driver.findElement(By.xpath("//td/div[contains(@data-testid,'30/12')]")).click();
-
-
-        // selecionar numero personas
-
-        driver.findElement(By.xpath("//div[@data-testid='structured-search-input-field-guests-button']")).click();
-
-
-        int numAdult = 3;
-        for (int i = 0; i < numAdult ; i++) {
-            driver.findElement(By.xpath("//button[@data-testid='stepper-adults-increase-button']")).click();
-        }
-
-        driver.findElement(By.xpath("//button[@data-testid='structured-search-input-search-button']")).click();
-
-       //driver.findElement(By.xpath("(//div[@data-testid='card-container'])[1]")).click();
-
-
-
-        /// cambiar de ventana
-
-        String originalWindow = driver.getWindowHandle();
-
-        // verificar que  no existan mas ventana abiertas
-        Assertions.assertEquals(driver.getWindowHandles().size(),1);
-
-        // seleccionar primer resultado
-        driver.findElement(By.xpath("(//div[@data-testid='card-container'])[1]")).click();
-
-        //esperar que se abra segunda vetanan
-        new WebDriverWait(driver, Duration.ofSeconds(3)).until(ExpectedConditions.numberOfWindowsToBe(2));
-
-        for(String windowHadle: driver.getWindowHandles()){
-            if(!originalWindow.contentEquals(windowHadle)){
-                driver.switchTo().window(windowHadle);
-                break;
-            }
-        }
-
-        driver.close();
-
-
-
-
-
-
-
-
-
+    void openPage() {
+        System.setProperty("webdriver.chrome.driver","src/test/resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get("https://www.airbnb.com.co/");
     }
 }
